e4817dbdf038c51bdeb2b4fbbb5ad2e4143e8b70
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2023-02-08 22:54:01 +0300

    Work

--- src/main/java/com/vetka/gateway/schema/bo/GraphQlEndpointInfo.java
+++ src/main/java/com/vetka/gateway/schema/bo/GraphQlEndpointInfo.java
@@ -4,12 +4,20 @@ import com.vetka.gateway.mgmt.graphqlendpoint.model.GraphQlEndpoint;
 import java.util.Set;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 
 @Getter
 @Builder
 public class GraphQlEndpointInfo {
 
+    @NonNull
     private final GraphQlEndpoint graphQlEndpoint;
 
+    @NonNull
     private final Set<String> queries, mutations, subscriptions;
+
+    @Override
+    public String toString() {
+        return "GraphQlEndpointInfo{" + "graphQlEndpoint=" + graphQlEndpoint + ", queries=" + queries.size() + ", mutations=" + mutations.size() + ", subscriptions=" + subscriptions.size() + '}';
+    }
 }
--- src/main/java/com/vetka/gateway/schema/service/GraphQlSchemaRegistryService.java
+++ src/main/java/com/vetka/gateway/schema/service/GraphQlSchemaRegistryService.java
@@ -3,8 +3,15 @@ package com.vetka.gateway.schema.service;
 import com.vetka.gateway.mgmt.graphqlendpoint.model.GraphQlEndpoint;
 import com.vetka.gateway.persistence.api.IPersistenceServiceFacade;
 import com.vetka.gateway.schema.bo.GraphQlEndpointInfo;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.idl.SchemaGenerator;
+import jakarta.annotation.Nullable;
 import jakarta.annotation.PostConstruct;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -38,7 +45,22 @@ public class GraphQlSchemaRegistryService {
     }
 
     private GraphQlEndpointInfo parse(@NonNull final GraphQlEndpoint graphQlEndpoint) {
-        log.info("parse graphQlEndpoint={}", graphQlEndpoint);
-        return GraphQlEndpointInfo.builder().graphQlEndpoint(graphQlEndpoint).build();
+        log.info("parsing graphQlEndpoint={}", graphQlEndpoint);
+
+        final var schema = SchemaGenerator.createdMockedSchema(graphQlEndpoint.getSchema());
+        final var result = GraphQlEndpointInfo.builder()
+                .graphQlEndpoint(graphQlEndpoint)
+                .queries(fieldNames(schema.getQueryType()))
+                .mutations(fieldNames(schema.getMutationType()))
+                .subscriptions(fieldNames(schema.getSubscriptionType()))
+                .build();
+        log.info("parsed {}", result);
+        return result;
+    }
+
+    private static Set<String> fieldNames(@Nullable final GraphQLObjectType src) {
+        return Optional.ofNullable(src)
+                .map(t -> t.getFields().stream().map(GraphQLFieldDefinition::getName).collect(Collectors.toSet()))
+                .orElse(Set.of());
     }
 }
