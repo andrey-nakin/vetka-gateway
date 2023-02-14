9205cfedcd991db4ca0d4481b7a6618d63c0d745
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2023-02-14 16:13:48 +0300

    Work

--- src/main/java/com/vetka/gateway/graphql/GraphQlSchemaMerger.java
+++ src/main/java/com/vetka/gateway/graphql/GraphQlSchemaMerger.java
@@ -1,5 +1,7 @@
 package com.vetka.gateway.graphql;
 
+import com.vetka.gateway.schema.service.GraphQlConstants;
+import graphql.language.ObjectTypeDefinition;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import graphql.schema.idl.errors.SchemaProblem;
@@ -14,8 +16,38 @@ public class GraphQlSchemaMerger {
         sdls.forEachOrdered(sdl -> {
             final var schemaParser = new SchemaParser();
             final var sdlRegistry = schemaParser.parse(sdl);
+
+            checkTypeConflict(typeDefinitionRegistry, sdlRegistry, GraphQlConstants.TYPE_QUERY);
+            checkTypeConflict(typeDefinitionRegistry, sdlRegistry, GraphQlConstants.TYPE_MUTATION);
+            checkTypeConflict(typeDefinitionRegistry, sdlRegistry, GraphQlConstants.TYPE_SUBSCRIPTION);
+
             typeDefinitionRegistry.merge(sdlRegistry);
         });
         return typeDefinitionRegistry;
+    }
+
+    private static void checkTypeConflict(final TypeDefinitionRegistry target, final TypeDefinitionRegistry source,
+            final String typeName) {
+
+        if (target.getType(typeName).isPresent() && source.getType(typeName).isPresent()) {
+            final var dest = (ObjectTypeDefinition) target.getType(typeName).get();
+            target.remove(dest);
+
+            final var src = (ObjectTypeDefinition) source.getType(typeName).get();
+            source.remove(src);
+
+            target.add(ObjectTypeDefinition.newObjectTypeDefinition()
+                    .name(dest.getName())
+                    .directives(dest.getDirectives())
+                    .fieldDefinitions(
+                            Stream.concat(dest.getFieldDefinitions().stream(), src.getFieldDefinitions().stream())
+                                    .toList())
+                    .description(dest.getDescription())
+                    .sourceLocation(dest.getSourceLocation())
+                    .comments(dest.getComments())
+                    .ignoredChars(dest.getIgnoredChars())
+                    .additionalData(dest.getAdditionalData())
+                    .build());
+        }
     }
 }
