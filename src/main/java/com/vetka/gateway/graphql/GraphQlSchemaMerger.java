e61876fb664f857390656e2666d9f76ac194fd42
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2023-02-14 13:14:35 +0300

    Work

--- src/main/java/com/vetka/gateway/graphql/GraphQlSchemaMerger.java
+++ src/main/java/com/vetka/gateway/graphql/GraphQlSchemaMerger.java
@@ -2,13 +2,14 @@ package com.vetka.gateway.graphql;
 
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import graphql.schema.idl.errors.SchemaProblem;
 import java.util.stream.Stream;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class GraphQlSchemaMerger {
 
-    public static TypeDefinitionRegistry merge(final Stream<String> sdls) {
+    public static TypeDefinitionRegistry merge(final Stream<String> sdls) throws SchemaProblem {
         final var typeDefinitionRegistry = new TypeDefinitionRegistry();
         sdls.forEachOrdered(sdl -> {
             final var schemaParser = new SchemaParser();
