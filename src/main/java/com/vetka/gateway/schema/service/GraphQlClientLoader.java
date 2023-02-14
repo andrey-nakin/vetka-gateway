3b13f6d4e0c75dcebd616f11729a03493eeee3c4
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2023-02-14 17:05:34 +0300

    Work

--- src/main/java/com/vetka/gateway/schema/service/GraphQlClientLoader.java
+++ src/main/java/com/vetka/gateway/schema/service/GraphQlClientLoader.java
@@ -8,6 +8,7 @@ import graphql.schema.DataFetchingEnvironment;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -26,30 +27,37 @@ public class GraphQlClientLoader implements MappedBatchLoader<DataFetchingEnviro
 
     @Override
     public CompletionStage<Map<DataFetchingEnvironment, Object>> load(final Set<DataFetchingEnvironment> keys) {
-        final var keyMap = keys.stream()
-                .collect(Collectors.toMap(
-                        k -> StringUtils.defaultIfBlank(k.getField().getAlias(), k.getField().getName()),
-                        Function.identity()));
+        return CompletableFuture.supplyAsync(() -> {
+                    final var keyMap = keys.stream()
+                            .collect(Collectors.toMap(
+                                    k -> StringUtils.defaultIfBlank(k.getField().getAlias(), k.getField().getName()),
+                                    Function.identity()));
 
-        final var query = GraphQlQueryBuilder.build(keys);
-        log.debug("Sending GraphQL request to {}: {}", graphQlEndpointInfo.getGraphQlEndpoint().getAddress(), query);
+                    final var query = GraphQlQueryBuilder.build(keys);
+                    log.debug("Sending GraphQL request to {}: {}", graphQlEndpointInfo.getGraphQlEndpoint().getAddress(),
+                            query);
 
-        final HttpHeaders httpHeaders;
-        if (!keys.isEmpty()) {
-            final GatewayLocalContext context = keys.iterator().next().getLocalContext();
-            httpHeaders = context.getRequestWrapper().request().getHeaders();
-        } else {
-            httpHeaders = HttpHeaders.EMPTY;
-        }
+                    final HttpHeaders httpHeaders;
+                    if (!keys.isEmpty()) {
+                        final GatewayLocalContext context = keys.iterator().next().getLocalContext();
+                        httpHeaders = context.getRequestWrapper().request().getHeaders();
+                    } else {
+                        httpHeaders = HttpHeaders.EMPTY;
+                    }
 
-        return transportService.request(httpHeaders, query, graphQlEndpointInfo).thenApply(response -> {
-            final var result = new HashMap<DataFetchingEnvironment, Object>();
-            final Map<String, Object> data = response.getData();
-            if (data != null) {
-                data.forEach((key, value) -> result.put(keyMap.get(key), value));
-            }
-            // TODO handle errors and extensions
-            return result;
-        });
+                    return new QueryData(keyMap, httpHeaders, query);
+                })
+                .thenCompose(queryData -> transportService.request(queryData.httpHeaders(), queryData.query(),
+                        graphQlEndpointInfo).thenApply(response -> {
+                    final var result = new HashMap<DataFetchingEnvironment, Object>();
+                    final Map<String, Object> data = response.getData();
+                    if (data != null) {
+                        data.forEach((key, value) -> result.put(queryData.keyMap().get(key), value));
+                    }
+                    // TODO handle errors and extensions
+                    return result;
+                }));
     }
+
+    private record QueryData(Map<String, DataFetchingEnvironment> keyMap, HttpHeaders httpHeaders, String query) {}
 }
