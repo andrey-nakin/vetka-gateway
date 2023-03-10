4d490cb1e2736e21d708569a82d2b10c5618a170
Andrey Nakin <andrey.nakin@gmail.com>
2023-03-10 22:42:39 +0300

    work

--- vetka-gateway-core/src/main/java/io/vetka/gateway/endpoint/GatewayRouter.java
+++ vetka-gateway-core/src/main/java/io/vetka/gateway/endpoint/GatewayRouter.java
@@ -2,6 +2,7 @@ package io.vetka.gateway.endpoint;
 
 import io.vetka.gateway.endpoint.properties.EndpointProperties;
 import io.vetka.gateway.schema.service.GraphQlConstants;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RequestPredicates;
@@ -12,11 +13,14 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration
 public class GatewayRouter {
 
+    private static final String DEFAULT_PATH = "/";
+
     @Bean
     public RouterFunction<ServerResponse> route(final EndpointProperties endpointProperties,
             final GatewayHttpHandler gatewayHttpHandler) {
 
-        return RouterFunctions.route(RequestPredicates.POST(endpointProperties.getPath())
-                .and(RequestPredicates.accept(GraphQlConstants.MEDIA_TYPE)), gatewayHttpHandler::handleRequest);
+        return RouterFunctions.route(
+                RequestPredicates.POST(StringUtils.defaultIfBlank(endpointProperties.getPath(), DEFAULT_PATH))
+                        .and(RequestPredicates.accept(GraphQlConstants.MEDIA_TYPE)), gatewayHttpHandler::handleRequest);
     }
 }
